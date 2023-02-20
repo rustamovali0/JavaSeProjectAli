6923eb758575680c619d802eb43f9c0539de902b
rustamovali0 <rustamovali012@gmail.com>
2023-02-20 18:46:51 +0400

    practise 3

--- VariablesPracticeHome/src/main/main.java
+++ VariablesPracticeHome/src/main/main.java
@@ -3,8 +3,8 @@ package main;
 public class main {
 	public static void main(String[] args) {
 		
-		boolean hasJob=false;
-		System.out.println("hasJob= "+hasJob);
+		char studentGrade='C';
+		System.out.println("studentGrade= "+studentGrade);
 		
 		
 	}
