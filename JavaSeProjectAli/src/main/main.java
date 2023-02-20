a25ecf6c4a1c8317a325b316601dd9ccbadae941
rustamovali0 <rustamovali012@gmail.com>
2023-02-20 18:34:40 +0400

    char short

--- JavaSeProjectAli/src/main/main.java
+++ JavaSeProjectAli/src/main/main.java
@@ -2,9 +2,12 @@ package main;
 
 public class main {
 
-   public static void main(String[] args) {
-	   
-	   char c='A';
-	   System.out.println(c);
-   }
+	public static void main(String[] args) {
+
+       char c= 'A';
+       short s = (short) c;
+       System.out.println(c);
+       System.out.println(s);
+
+	}
 }
