707391f882d3b50a3742988940e31eb1dc8e1a48
rustamovali0 <rustamovali012@gmail.com>
2023-02-27 10:28:42 +0400

    practise1/7

--- JavaSeProjectAli/src/JavaSeProjectAli/main.java
+++ JavaSeProjectAli/src/JavaSeProjectAli/main.java
@@ -4,9 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		char c='D';
-		int a=(int)c;
-        System.out.println(a);
+		int a=123;
+		char c=(char)a;
+        System.out.println(c);
 	}
 
 }
