aaff099a0996cb185f4d373edb993bdb973cefab
rustamovali0 <rustamovali012@gmail.com>
2023-02-27 10:19:29 +0400

    practise1/3

--- JavaSeProjectAli/src/JavaSeProjectAli/main.java
+++ JavaSeProjectAli/src/JavaSeProjectAli/main.java
@@ -4,9 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		short s=589;
-		byte a=(byte)s;
-        System.out.println(a);
+		long l=458;
+		int a=(int)l;
+        System.out.println(l);
 	}
 
 }
