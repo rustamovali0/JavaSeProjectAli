bbda57439ada498c47b4581f50f62f63278076c2
rustamovali0 <rustamovali012@gmail.com>
2023-02-27 11:58:41 +0400

    homework2/for/tek

--- JavaSeProjectAli/src/JavaSeProjectAli/main.java
+++ JavaSeProjectAli/src/JavaSeProjectAli/main.java
@@ -5,12 +5,10 @@ public class main {
 	public static void main(String[] args) {
 		
 		int i=0;
-		
-		for(i=15;i<=40;i++)
+		for(i=20;i<=50;i++) 
 		{
-		
-        System.out.println(i);
-	}
-		
+		if(i%2!=0) 
+		  System.out.println(i);
+		}
 }
 }
