9265087baa2cbd900fdeb928b0e153888061368a
rustamovali0 <rustamovali012@gmail.com>
2023-02-28 10:38:39 +0400

    for dovru 50 den 20 ye azalma

--- JavaSeProjectAli/src/JavaSeProjectAli/main.java
+++ JavaSeProjectAli/src/JavaSeProjectAli/main.java
@@ -4,10 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		int i=0;
-		for(i=20;i<=50;i++) 
+int i=0;
+		
+		for(i=50;i>=20;i--) 
 		{
-		if(i%2==0) 
+		 
 		  System.out.println(i);
 		}
 }
