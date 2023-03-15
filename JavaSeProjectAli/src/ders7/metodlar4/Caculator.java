1c7a2dd01f447bdc8577700a64104dabcb8512c6
rustamovali0 <rustamovali012@gmail.com>
2023-03-15 20:55:08 +0400

    homework1

---
+++ JavaSeProjectAli/src/ders7/metodlar4/Caculator.java
@@ -0,0 +1,15 @@
+package ders7.metodlar4;
+
+public class Caculator {
+	public static void main(String[] args) {
+		//average(3,7);
+	}
+		
+	void average(int a,int b) {
+	    int c=(a+b)/2;
+	    System.out.println(c);
+		
+		
+	}
+
+}
---
+++ JavaSeProjectAli/src/ders7/metodlar4/MethodsReturn.java
@@ -0,0 +1,17 @@
+package ders7.metodlar4;
+
+public class MethodsReturn {
+public static void main(String[] args) {
+	
+	Caculator a=new Caculator();
+	a.average(3,7);
+	a.average(5,7);
+}
+
+
+
+
+
+
+
+}
