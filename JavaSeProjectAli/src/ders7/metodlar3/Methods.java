45f0a41ebeadb275a4e684ecce6e0f67fa37b1cf
rustamovali0 <rustamovali012@gmail.com>
2023-03-17 14:00:40 +0400

    methods.homework1

---
+++ JavaSeProjectAli/src/ders7/metodlar3/Methods.java
@@ -0,0 +1,15 @@
+package ders7.metodlar3;
+
+public class Methods {
+
+	void printNumbers(int begin,int end) {
+		
+		for(int i=begin;i<=end;i++) {
+			
+			System.out.println(" "+i);
+		
+
+		}
+
+}
+}
---
+++ JavaSeProjectAli/src/ders7/metodlar3/main.java
@@ -0,0 +1,13 @@
+package ders7.metodlar3;
+
+public class main {
+	public static void main(String[] args) {
+		
+		Methods M=new Methods();
+		M.printNumbers(7, 10);
+		System.out.println("***");
+		M.printNumbers(7, 15);
+		System.out.println("****");
+		M.printNumbers(15, 25);
+	}
+}
